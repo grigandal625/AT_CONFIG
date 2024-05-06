8b39ed4ce096621de3a1c26ccfd201659dcd95d7
grigadnal625 <grigandal625@gmail.com>
2024-05-07 02:15:46 +0300

    new version

--- Makefile
+++ Makefile
@@ -14,18 +14,16 @@ test: dev
 
 clean:
 	@rm -rf .pytest_cache/ .mypy_cache/ junit/ build/ dist/
-	
-build: clean
-	pipenv install --dev wheel
-	pipenv run python setup.py bdist_wheel
 stable:
 	cp dist/at_config-*.*-py3-none-any.whl dist/at_config-stable-py3-none-any.whl
 latest:
 	cp dist/at_config-*.*-py3-none-any.whl dist/at_config-latest-py3-none-any.whl
+build: clean
+	pipenv install --dev wheel
+	pipenv run python setup.py bdist_wheel
 incver:
 	pipenv run python ./scripts/incver.py
 requirements:
 	pipenv run python -m pip freeze | sed '/^-e git/d' > requirements.txt
-	pipenv run python ./scripts/fix_requirements.py
 rabbit:
 	docker run --rm -p 15672:15672 -p 5672:5672 rabbitmq:management
--- setup.py
+++ setup.py
@@ -10,23 +10,21 @@ root = os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir))
 os.chdir(root)
 
 
-# old
 def read_pipenv_dependencies(fname):
     filepath = os.path.join(os.path.dirname(__file__), fname)
     with open(filepath) as lockfile:
         lockjson = json.load(lockfile)
         return [dependency for dependency in lockjson.get('default')]
 
-# new
 
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]  
+# with pathlib.Path('requirements.txt').open() as requirements_txt:
+#     install_requires = [
+#         str(requirement)
+#         for requirement
+#         in pkg_resources.parse_requirements(requirements_txt)
+#     ]  
 
-VERSION = os.getenv('PACKAGE_VERSION', '0.0.dev7')
+VERSION = os.getenv('PACKAGE_VERSION', '0.0.dev8')
 
 if __name__ == '__main__':
     setup(
@@ -35,5 +33,5 @@ if __name__ == '__main__':
         packages=find_packages(where='src'),
         package_dir={'': 'src'},
         description='AT-TECHNOLOGY configuration structures.',
-        install_requires=install_requires
+        install_requires=read_pipenv_dependencies('Pipfile.lock')
     )
